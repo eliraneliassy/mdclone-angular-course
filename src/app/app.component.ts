546dbfae2c37e7e8601f337985139bbe2ee50ee4
Eliran Eliassy <eliran.eliassy@gmail.com>
2022-11-20 11:50:31 +0200

    app component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,9 +7,23 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  book: Book = {
-    title: 'Harry Potter',
-    imagePreviewUrl: `http://prodimage.images-bn.com/pimages/9780590353427_p0_v2_s1200x630.jpg`,
-    price: 100
-  }
+  
+
+  books: Book[] = [
+    {
+      title: 'Harry Potter',
+      imagePreviewUrl: `http://prodimage.images-bn.com/pimages/9780590353427_p0_v2_s1200x630.jpg`,
+      price: 100
+    },
+    {
+      title: 'Lord of the rings',
+      imagePreviewUrl: `https://m.media-amazon.com/images/I/51kfFS5-fnL._AC_SY780_.jpg`,
+      price: 120
+    },
+    {
+      title: 'Game of thornes',
+      imagePreviewUrl: `https://m.media-amazon.com/images/I/51IHAPK5fsL._AC_SY780_.jpg`,
+      price: 200
+    }
+  ]
 }
